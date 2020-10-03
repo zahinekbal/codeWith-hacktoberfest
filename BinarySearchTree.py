74fe6a66ad7aed9c5c799665c8ddedb83a39c0a8
bEERy <siradanai27@gmail.com>
2020-10-03 22:57:26 +0700

    Added binary search tree in python

---
+++ BinarySearchTree.py
@@ -0,0 +1,39 @@
+# Python Binary Search Tree program
+
+# Class that represents an individual node in a BST
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+# function to insert a new node with the given key
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+# function to do inorder tree traversal
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+r = Node(50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+
+inorder(r)

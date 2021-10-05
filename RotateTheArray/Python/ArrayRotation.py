3ae309b8048c240f6fc1a701ba424e49c0e0f6ab
Harsh Mehta <73291466+1n40@users.noreply.github.com>
2021-10-05 12:16:42 +0530

    Refactored code

--- RotateTheArray/Python/ArrayRotation.py
+++ RotateTheArray/Python/ArrayRotation.py
@@ -1,23 +1,19 @@
 #Python program for Array Rotation   
 arr = [1, 2, 3, 4, 5];     
 #n is the int value that determine times of rotation   
-n = 3;    
-     
-#Rotating the array by n times   
-for i in range(0, n):    
-    #Storing the first element of the array in temp  
-    temp = arr[0];    
-        
-    for j in range(0, len(arr)-1):    
-        #Shift element by one position   
-        arr[j] = arr[j+1];    
-            
-    #Adding temp in last index of arr    
-    arr[len(arr)-1] = temp;    
-     
-print();    
-     
-#Printing Array after rotation    
-print("Rotated Array is: ");    
-for i in range(0, len(arr)):    
-    print ("%d" % arr[i], end=" "),  
+n = 3;
+
+#Initilize an empty list
+temp = []
+
+#Store the Non-rotating elements to temp 
+for i in range(n, len(arr)):
+    temp.append(arr[i])
+
+#Append the rotated elements to temp
+for i in range(n):
+    temp.append(arr[i])
+
+#Printing the rotated array
+for i in temp:
+    print(i, end=" ")
